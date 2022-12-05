746cb7aedfa79ef4672f7f5ed691284a763b0ba3
DHayes23 <d.hayesdeveloper@gmail.com>
2022-12-05 16:45:11 +0000

    Revert message change

--- python-front-and-back/frontend/app.py
+++ python-front-and-back/frontend/app.py
@@ -10,7 +10,7 @@ def home():
     backend_hostname = requests.get("http://backend:5001/hostname")
     random = requests.get("http://backend:5001/random")
     colour = "red"
-    return f"<body style='background-color:red;'>\n<h1>Hello Denis.</h1>\n\n<h2>I'm currently running in {hostname}.</h2>\n\n<h2>The backend I'm chatting with is running in {backend_hostname.text}</h2>\n\n<h3>The backend gave me this to show you: {random.text}</h3>"
+    return f"<body style='background-color:red;'>\n<h1>Hello friend.</h1>\n\n<h2>I'm currently running in {hostname}.</h2>\n\n<h2>The backend I'm chatting with is running in {backend_hostname.text}</h2>\n\n<h3>The backend gave me this to show you: {random.text}</h3>"
 
 if __name__=="__main__":
 	app.run(host = "0.0.0.0", port = 5000, debug = True)
